0546b0f3697fc8c7348d38c6f40faa38a3f933d3
jase <sebringjulia@gmail.com>
2019-09-12 15:27:47 -0700

    Read csv

--- homework/03_python-challenge/PyBank/test.py
+++ homework/03_python-challenge/PyBank/test.py
@@ -2,12 +2,18 @@
 
 #Dependencies
 import os
+import csv
 
-#Store file in memory
-file = "budget_data.csv"
+#Store file path
+csv_path = os.path.join("budget_data.csv")
 
 #Read csv
-with open(file, 'r') as text:
-    data = text.read()
+with open(csv_path, newline = "") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ",")
 
-print(data)
+    print(csv_reader)
+
+    csv_header = next(csv_reader)
+
+    for row in csv_reader:
+        print(row)
