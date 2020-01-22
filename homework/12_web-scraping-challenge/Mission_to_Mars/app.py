8e4a445b36f7bed330ee3aa93441a73920c9631d
jase <sebringjulia@gmail.com>
2020-01-22 15:09:55 -0800

    Pymongo connection created

--- homework/12_web-scraping-challenge/Mission_to_Mars/app.py
+++ homework/12_web-scraping-challenge/Mission_to_Mars/app.py
@@ -1,12 +1,49 @@
-from Flask import flask, render_template
+from flask import Flask, render_template
+import pymongo
 
 # create instance of Flask app
 app = Flask(__name__)
 
+# Create connection variable
+conn = "mongodb://localhost:27017"
+
+# Pass connection to the pymongo instance
+client = pymongo.MongoClient(conn)
+
+# Connect to a database
+db = client.mars_db
+
+# Drops collection if available to remove duplicates
+db.news.drop()
+
+# Creates a collection in the database and inserts two documents
+db.news.insert_many (
+    [
+        {
+            'player': 'Jessica',
+            'position': 'Point Guard'
+        },
+        {
+            'player': 'John',
+            'position': 'Center'
+        }
+    ]
+)
+
 # create route that renders index.html template
 @app.route("/")
 def echo():
-    return render_template("index.html", #)
+    # Store the entire news collection in a list
+    news = list(db.news.find())
+    #text = "whatup"
+
+    # Return the template with the news list passed in
+    return render_template("index.html", news=news)
+
+# create route that renders index.html template
+@app.route("/scrape")
+def scrape():
+    return render_template("scrape.html", text="Scraped Data")
 
 if __name__ == "__main__":
     app.run(debug=True)
